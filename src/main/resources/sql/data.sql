4747460e38fe4f254a2fe0e50482f350043f1f6d
liuguoqing123 <279189435@qq.com>
2024-01-15 22:31:09 +0800

    [lewis] update SQL file

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,11 +1,29 @@
-IF OBJECT_ID ('score') IS  NULL
-CREATE TABLE dbo.score
-	(
-	id          INT IDENTITY NOT NULL,
-	stuNo  VARCHAR (255),
-	name   VARCHAR (255),
-	score     VARCHAR (255),
-	classes VARCHAR (255),
-	tpye  VARCHAR (255)
-	CONSTRAINT pk_score_tab PRIMARY KEY (id)
-	);
+IF OBJECT_ID ('scores') IS NULL
+CREATE TABLE dbo.score(
+id INT IDENTITY NOT NULL,
+subject VARCHAR (10),
+score INT,
+tpye VARCHAR (10),
+stuNo VARCHAR（20),
+create_by VARCHAR (50),
+update_by VARCHAR (50),
+create_time DATETIME,
+update_time DATETIME
+CONSTRAINT pk_score_tab PRIMARY KEY (id)
+)
+
+
+IF OBJECT_ID ('student') IS NULL
+CREATE TABLE dbo.score(
+id INT IDENTITY NOT NULL,
+stuNo VARCHAR (20),
+name VARCHAR(50),
+classes VARCHAR (10),
+dob VARCHAR (26),
+phone_number VARCHAR（20),
+create_by VARCHAR (50),
+update_by VARCHAR (50),
+create_time DATETIME,
+update_time DATETIME,
+CONSTRAINT pk_student_tab PRIMARY KEY (id)
+)
